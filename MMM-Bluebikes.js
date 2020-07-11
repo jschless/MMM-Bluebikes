2d166f4c47cc3cfc37610b85f51f54bd63d8a76e
Joe Schlessinger <joe.c.schlessinger@gmail.com>
2020-07-11 10:14:51 -0400

    working

--- MMM-Bluebikes.js
+++ MMM-Bluebikes.js
@@ -1,153 +1,167 @@
-Module.register("MMM-Bluebikes", {
+/* global Module, Log */
+
+/*
+ * Magic Mirror Module: MMM-Bubi (https://github.com/balassy/MMM-Bubi)
+ * By György Balássy (https://www.linkedin.com/in/balassy)
+ * MIT Licensed.
+ */
+
+Module.register('MMM-Bluebikes', {
     defaults: {
-        apikey: "",
-        updateInterval: 10, // In seconds
-	url: 'https://gbfs.bluebikes.com/gbfs/en/station_status.json',
-        stations: [ "Fan Pier" ],
-        maxEntries: 8,
-        maxTime: 0,
-        fade: true,
-        fadePoint: 0.25, // Start on 1/4th of the list.
-        showFullName: false,
-        colorIcons: false,
-        showAlerts: false,  // works but styling needs help
-        hideEmptyAlerts: false,
+	updateInterval: 30000,
+	showPlaceName: true,
+	url: "https://gbfs.bluebikes.com/gbfs/en/station_status.json",
+	places: [
+	    {id: "76", name: "City Hall"},
+	    {id: "116", name: "359 Broadway"},
+	    {id: "107", name: "TPP"}
+	]
+    },
+    
+    viewModel: {
+	places: []
     },
 
-    getStyles: function() {
-        return ["font-awesome.css", "MMM-MBTA.css"];
+    requiresVersion: '2.1.0',
+
+    getStyles() {
+	return [
+	    'MMM-Bluebikes.css'
+	];
     },
 
-    getHeader: function() {
-        return this.data.header + " " + this.config.stations[0];
+    start() {
+	const self = this;
+	this.hasData = false;
+
+	this._getData(() => self.updateDom());
+
+	setInterval(() => {
+	    self._getData(() => self.updateDom());
+	}, this.config.updateInterval);
     },
 
-    getScripts: function () {
-        return ["moment.js", "https://code.jquery.com/jquery-3.3.1.min.js"];
-    },
+    getDom() {
+	const wrapper = document.createElement('div');
 
-    start: function() {
-        // API abuse prevention
-        this.config.fadePoint = Math.max(this.config.fadePoint, 0);
-        this.config.updateInterval = Math.max(this.config.updateInterval, 10);
+	if (this.viewModel) {
+	    const tableEl = document.createElement('table');
+	    tableEl.classList = 'small';
 
-        this.loaded = false;
-        this.getStations = fetch('modules/MMM-Bluebikes/stations-formatted.json')
-            .then(res => res.json())
-            .then(stationDict => this.config.stations.map(friendlyName => stationDict[friendlyName]));
+	    if (this.config.align === 'left') {
+		tableEl.classList += ' align-left';
+	    }
 
-        this.stationData = [];
-    },
+	    if (this.config.align === 'right') {
+		tableEl.classList += ' align-right';
+	    }
 
-    getDom: function() {
-        var wrapper = document.createElement("div");
+	    const rowEl = document.createElement('tr');
+	    const symbolEl = document.createElement('th');
+	    symbolEl.classList = 'fa fa-bicycle dimmed symbol-cell';
+	    rowEl.appendChild(symbolEl);
+	    
+	    const countEl = document.createElement('th');
+	    countEl.classList = 'count-cell';
+	    countEl.innerText = '# Bikes';
+	    rowEl.appendChild(countEl);
 
-        if (!this.loaded) {
-            wrapper.innerHTML += "LOADING";
-            wrapper.className = "dimmed light small";
-        }
+	    const dockEl = document.createElement('th');
+	    dockEl.classList = 'count-cell';
+	    dockEl.innerText = '# Docks';
+	    rowEl.appendChild(dockEl);
 
-        var table = document.createElement("table");
-        table.className = "small";
+	    
+	    const nameEl = document.createElement('th');
+	    nameEl.classList = 'dimmed small name-cell';
+	    nameEl.innerText = 'Location';
+	    rowEl.appendChild(nameEl);
+	    tableEl.appendChild(rowEl);
+	    
+	    this.viewModel.places.forEach((place) => {
+		const rowEl = document.createElement('tr');
 
-        // When there are no predictions
-        if (!this.stationData.length) {
-            var row = document.createElement("tr");
-            table.appendChild(row);
+		const symbolEl = document.createElement('td');
+		symbolEl.classList = 'fa fa-bicycle dimmed symbol-cell';
+		rowEl.appendChild(symbolEl);
 
-            // Icon
-            var symbolCell = document.createElement("td");
-            symbolCell.className = "fa fa-times-circle";
-            symbolCell.style.cssText = "padding-right: 10px";
-            if (this.config.colorIcons) {
-                    symbolCell.className += " red";
-                }
-            row.appendChild(symbolCell);
+		const countEl = document.createElement('td');
+		countEl.classList = 'count-cell';
+		countEl.innerText = place.numberOfBikes;
+		rowEl.appendChild(countEl);
 
-            var descCell = document.createElement("td");
-            descCell.innerHTML = "Nothing coming soon...";
-            descCell.className = "align-left bright";
-            row.appendChild(descCell);
-        } else {
-            for (let station of this.stationData) {
-                var row = document.createElement("tr");
-                table.appendChild(row);
-                              
-                descCell.className = "align-left bright";
-                row.appendChild(descCell);
+		const dockEl = document.createElement('td');
+		dockEl.classList = 'count-cell';
+		dockEl.innerText = place.numberOfDocks;
+		rowEl.appendChild(dockEl);
 
-                // number available
-                if (true) {
-                    var arrTimeCell = document.createElement("td");
-		    arrTimeCell.innerHTML = station.num_bikes_available
-                    row.appendChild(arrTimeCell);
-                }
-            }
 
-            var startingPoint = this.stationData.length * this.config.fadePoint;
-            var steps = this.stationData.length - startingPoint;
-            for (let i = 0; i < this.stationData.length; i++) {
-                // Stolen from default modules to ensure style is identical. Thanks MichMich <3
-                if (this.config.fade && this.config.fadePoint < 1) {
-                    if (i >= startingPoint) {
-                        var currentStep = i - startingPoint;
-                        table.rows[i].style.opacity = 1 - (1 / steps * currentStep);
-                    }
-                }
-            }
-        };
+		const nameEl = document.createElement('td');
+		nameEl.classList = 'dimmed small name-cell';
+		nameEl.innerText = place.name;
+		rowEl.appendChild(nameEl);
 
-        wrapper.appendChild(table);
+		tableEl.appendChild(rowEl);
+	    });
 
-        // Don't start the update loop on first init
-        if (this.loaded) {
-            this.scheduleUpdate();
-        }
-    },
+	    wrapper.appendChild(tableEl);
 
-    notificationReceived: function(notification) {
-        if (notification === "DOM_OBJECTS_CREATED") {
-            Log.log(this.name + " received a system notification: " + notification);
-            this.fetchData();
-        }
-    },
-
-    scheduleUpdate: function(amt) {
-        var interval = amt || this.config.updateInterval;
-        var self = this;
-        setTimeout(function() {
-            self.fetchData();
+	    const clearfixEl = document.createElement('div');
+	    clearfixEl.classList = 'clearfix';
+	    wrapper.appendChild(clearfixEl);
+	} else {
+	    const loadingEl = document.createElement('span');
+	    loadingEl.innerHTML = this.translate('LOADING');
+	    loadingEl.classList = 'dimmed small';
+	    wrapper.appendChild(loadingEl);
 	}
-	, interval * 1000);
+
+	return wrapper;
     },
 
-    // params: updateDomAfter: boolean, whether or not to call updateDom() after processing data.
-    getJSON: function(url, callback) {
-	var MBTARequest = new XMLHttpRequest();
-        MBTARequest.open('GET', url, true);
-        MBTARequest.responseType = 'json';
-      	MBTARequest.onload = function() {
-	    var status = MBTARequest.status;
-	    if (status === 200) {
-		callback(null, MBTARequest.response);
-	    } else {
-		callback(status, MBTARequest.response);
+    _getData(onCompleteCallback) {
+	const self = this;
+
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', this.config.url, true);
+	xhr.onreadystatechange = function onReadyStateChange() {
+	    if (this.readyState === 4) {
+		if (this.status === 200) {
+		    self._processResponse(this.response);
+		    onCompleteCallback();
+		} else {
+		    Log.error(self.name, `MMM-Bluebikes: Failed to load data. XHR status: ${this.status}`);
+		}
 	    }
 	};
-        MBTARequest.send();
+
+	xhr.send();
     },
 
-    fetchData: function() {    
-	getJSON(this.url,
-		function(err, data) {
-		    if (err !== null){
-    			alert('something went wrong');
-		    } else {
-			for (let station in this.getStations) {
-			    this.stationData.push(data.data.stations[station])
-			}
-		    }
-		this.loaded = true
-		});
-    },
+    _processResponse(responseBody) {
+	const response = JSON.parse(responseBody);
+	const places = response.data.stations;
+	this.viewModel.places = [];
+
+	
+	for (let i = 0; i < this.config.places.length; i++) {
+	    const place = places.find(p => p.station_id === this.config.places[i].id);
+
+	    // const place = places[this.config.places[i].id];
+	    Log.log(place);
+	    this.viewModel.places.push({
+		name: this.config.showPlaceName
+		    ? this.config.places[i].name// || place.name
+		    : null,
+		numberOfBikes: place.num_bikes_available,
+		numberOfDocks: place.num_docks_available
+	    });
+	}
+
+	if (!this.hasData) {
+	    this.updateDom();
+	}
+
+	this.hasData = true;
+    }
 });
--- README.md
+++ README.md
@@ -1,2 +1,119 @@
-# MMM-Bluebikes
-Magic Mirror Bluebikes module
+# MMM-Bubi
+
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/) to display the number of available bikes on the selected stations of the Budapest public bike system (aka MOL Bubi).
+
+## Features
+
+By default this module displays the number of available bikes and the official names of the specified stations:
+
+![Default](https://raw.githubusercontent.com/balassy/MMM-Bubi/master/doc/screenshot-default.png)
+
+You can configure the module to display a custom (typically shorter) names for the stations:
+
+![With custom station names](https://raw.githubusercontent.com/balassy/MMM-Bubi/master/doc/screenshot-custom-place-name.png)
+
+If you wish, you can completely remove the station names:
+
+![With hidden station names](https://raw.githubusercontent.com/balassy/MMM-Bubi/master/doc/screenshot-no-place-name.png)
+
+For updates, please check the [CHANGELOG](https://github.com/balassy/MMM-Bubi/blob/master/CHANGELOG.md).
+
+## Using the module
+
+To use this module follow these steps:
+
+1. Clone this repository to the `modules` folder of your MagicMirror:
+
+```bash
+git clone https://github.com/balassy/MMM-Bubi.git
+```
+
+2. Add the following configuration block to the modules array in the `config/config.js` file:
+
+```js
+var config = {
+  modules: [
+    {
+      module: 'MMM-Bubi',
+      position: 'top_right',
+      config: {
+        updateInterval: 600000, // 10 minutes in milliseconds
+        showPlaceName: true,
+        align: 'left',
+        places: [
+          { id: 1758935, name: 'MOMKult' },
+          { id: 1758923, name: 'MOM Park' },
+          { id: 366538,  name: 'BAH csomópont' }
+        ]
+      }
+    }
+  ]
+}
+```
+
+## Configuration options
+
+| Option                 | Description
+|------------------------|-----------
+| `places`               | **REQUIRED** The list of Bubi stations to display. The unique `id` of the station is used to lookup the data from the webservice. The `name` is optional, and can be used to display a custom name for the station instead of the official name if the `showPlaceName` option is set to `true`. <br><br> **Type:** `Array<{ id: number, name: string }>` <br>**Default value:** 3 preconfigured stations
+| `showPlaceName`        | *Optional* Specifies whether the module should display not only the number of available bikes for each station, but also the names of the stations. If the `name` property is set in the `places` array, then it will be displayed, otherwise the official names of the stations will be rendered onto the Mirror.<br><br> **Type:** `boolean` <br>**Default value:** `true`
+| `align`                | *Optional* Determines how the text is aligned within the module. Set this to `left` if the module is displayed on left side of the mirror, or to `right` if you positioned this module to the right column of the mirror.<br><br>**Type:** `string`<br>**Possible values**: `'left'` or `'right'`<br>**Default value:** `'left'`
+| `updateInterval`       | *Optional* The frequency of how often the module should query the number of available bikes from the webservice. <br><br>**Type:** `int` (milliseconds) <br>**Default value:** `600000` milliseconds (10 minutes)
+
+## How to get the place ID
+
+In the `id` property of the objects in the `places` array of the configuration settings you have to specify the unique identifier of the station for which the module should display the bike availability.
+
+To obtain the unique identifier of the station, follow these steps:
+
+1. Navigate to the [MOL Bubi homepage](https://molbubi.bkk.hu/) with your favorite webbrowser.
+
+2. Use the map on the page to find your favorite MOL Bubi station and click its icon.
+
+3. Note the official name of the station, e.g. `1201-BAH csomópont`.
+
+4. Navigate to the https://bubi.nextbike.net/maps/nextbike-live.xml?&domains=mb URL and use the browser's find function (CTRL+F) to locate the place by its name.
+
+![Locate the station by its name](https://raw.githubusercontent.com/balassy/MMM-Bubi/master/doc/find-place-id.png)
+
+5. Find the `uid` attribute in the same row, it is the unique identifier of the station.
+
+## How it works
+
+This module periodically sends requests from the browser window of the MagicMirror Electron application to the public [NextBike API](https://bubi.nextbike.net/maps/nextbike-live.json?&domains=mb). The NextBike API is free, and it does NOT require any login or API key.
+
+You can see an XML example by visiting this URL: https://bubi.nextbike.net/maps/nextbike-live.xml?&domains=mb
+
+## Contribution
+
+Although for operation this module does not depend on any other module, if you would like to contribute to the codebase, please use the preconfigured linters to analyze the source code before sending a pull request. To run the linters follow these steps:
+
+1. Install developer dependencies:
+
+```bash
+npm install
+```
+
+2. Install Grunt:
+
+```bash
+npm install -g grunt
+```
+
+3. Use Grunt to run all linters:
+
+```bash
+grunt
+```
+
+## Got feedback?
+
+Your feedback is more than welcome, please send your suggestions, feature requests or bug reports as [Github issues](https://github.com/balassy/MMM-Bubi/issues).
+
+## Acknowledments
+
+Many thanks to [Michael Teeuw](https://github.com/MichMich) for creating and maintaining the [MagicMirror²](https://github.com/MichMich/MagicMirror/) project fully open source.
+
+## About the author
+
+This project is created and maintaned by [György Balássy](https://www.linkedin.com/in/balassy).
